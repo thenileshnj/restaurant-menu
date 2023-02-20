3afcdae617501b77d6644ea8ed0719f1dcd5af4d
Nilesh Nj <nileshbjadhawar@gmail.com>
2023-02-20 17:33:20 +0530

    Styles updation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,56 +1,72 @@
-import React, { useState } from 'react';
-import MenuLi from "../data/MenuLi"
-import MenuCard from './MenuCard';
-
-
-
-
+import React, { useState } from "react";
+import MenuLi from "../data/MenuLi";
+import MenuCard from "./MenuCard";
 
 const HomePage = () => {
+  const uniqueCategory = [
+    ...new Set(
+      MenuLi.map((data) => {
+        return data.category;
+      })
+    ),
+  ];
 
-const uniqueCategory = [ ...new Set( MenuLi.map((data) => {
-  return data.category;
-}))]
+  const filterItem = (data) => {
+    const updatedList = MenuLi.filter((e) => {
+      return e.category === data;
+    });
+    setMenuData(updatedList);
+  };
 
-
-const filterItem = (data) => {
-  const updatedList = MenuLi.filter((e) => {
-    return e.category === data;
-  })
-  setMenuData(updatedList);
-}
-
-console.log(uniqueCategory);
-
+  console.log(uniqueCategory);
 
   const [menuData, setMenuData] = useState(MenuLi);
   console.log(menuData);
 
-
-
-
   return (
     <>
-     <div className='w-full px-auto py-2 flex justify-center bg-purple-600 text-white'>
-      <h2 className='font-bold text-2xl'> Restaurant Menu Card  </h2>
-     </div>
-
-     {/* Menu category items  */}
-      <div className='flex flex-wrap pt-10 gap-4 justify-center '>
-            {
-              uniqueCategory.map((data) => {
-                return <div className='bg-purple-600 p-3 rounded-l' onClick={() => filterItem(data)}> {data} </div>
-              })
-            }
-            <div className='bg-purple-600 p-2 rounded-l' onClick={()=> setMenuData(MenuLi)}> All Items  </div>
+      <div className="w-full px-auto py-2 flex justify-center bg-purple-600 text-white">
+        <h2 className="font-bold text-2xl"> Restaurant Menu Card </h2>
       </div>
 
+      {/* Menu category items  */}
+      <div className="flex flex-wrap pt-10 gap-4 justify-center font-bold text-xl italic">
+        {uniqueCategory.map((data) => {
+          return (
+            <div
+              className="bg-purple-600 p-3 rounded-l"
+              onClick={() => filterItem(data)}
+            >
+              {" "}
+              {data}{" "}
+            </div>
+          );
+        })}
+        <div
+          className="bg-purple-600 p-2 rounded-l"
+          onClick={() => setMenuData(MenuLi)}
+        >
+          
+          All Items 
+        </div>
+      </div>
 
-     <div className='flex flex-wrap justify-center mx-auto gap-3 mt-12  w-4/5 h-24'>
-          <MenuCard menuData={menuData} />
-     </div>
+      <div className="flex flex-wrap justify-center mx-auto gap-3 mt-12  w-4/5 mb-16">
+        <MenuCard menuData={menuData} />
+      </div>
+
+      <footer
+        className="bg-black
+             text-lg text-white text-center
+             fixed
+             inset-x-0
+             bottom-0  
+             p-1 mt-4"
+      >
+        <p>Design And Developed by <a href="https://www.linkedin.com/in/thenileshnj/" target="_blank" rel="noopener noreferrer"> Nilesh Nj </a> with ❤️ & 🧋</p>
+      </footer>
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Thali from "../assets/Thali.jpeg";
 
-
 const MenuCard = ({ menuData }) => {
   return (
     <>
       {menuData.map((data) => {
         return (
-          <div className="p-4 bg-purple-600 rounded-xl m2 w-auto" key={data.id}>
+          <div
+            className="p-4 text-white font-bold bg-purple-600 rounded-xl m2 w-auto"
+            key={data.id}
+          >
             <img className="w-64 h-auto rounded-lg" src={Thali} alt="" />
-            <h3>{data.itemName}</h3>
-            <p>Price {data.price}</p>
-            <button className="bg-white px-2 py-1 rounded-xl hover:bg-yellow-400 to-orange-600">
+            <div className="mx-2 my-4 flex  justify-between">
+              <h3>{data.itemName}</h3>
+              <p>Price {data.price}</p>
+            </div>
+            <button className="bg-green-500 px-2 py-1 rounded-xl hover:bg-yellow-400 ">
               Add To Card
             </button>
           </div>
